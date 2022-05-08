8b5c92013126880d23a9b931835d4ccd74958b5f
Raul <jimenezraul1981@gmail.com>
2022-05-07 20:01:26 -0400

    add reaction form

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -40,3 +40,36 @@ export const ADD_FRIEND = gql`
     }
   }
 `;
+
+// Add thought
+export const ADD_THOUGHT = gql`
+  mutation addThought($thoughtText: String!) {
+    addThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      createdAt
+      username
+      reactionCount
+      reactions {
+        _id
+      }
+    }
+  }
+`;
+
+// Add reaction
+
+export const ADD_REACTION = gql`
+  mutation addReaction($thoughtId: ID!, $reactionBody: String!) {
+    addReaction(thoughtId: $thoughtId, reactionBody: $reactionBody) {
+      _id
+      reactionCount
+      reactions {
+        _id
+        reactionBody
+        createdAt
+        username
+      }
+    }
+  }
+`;
